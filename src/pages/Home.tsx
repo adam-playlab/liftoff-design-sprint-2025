d51cc2a9f14777a627374323373cae9b792ae3f0
Adam Murray <adam@playlab.ai>
2025-12-03 17:14:56 -0800

    tweak layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <>
       {/* Hero Section */}
-      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
+      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8">
         <div className="text-center">
-          <h1 className="text-6xl sm:text-7xl font-black mb-4 bg-linear-to-r from-indigo-600 via-purple-600 to-pink-500 bg-clip-text text-transparent drop-shadow-lg">
+          <h1 className="text-6xl sm:text-7xl font-black mb-8 bg-linear-to-r from-indigo-600 via-purple-600 to-pink-500 bg-clip-text text-transparent drop-shadow-lg">
             LifeLab
           </h1>
           <h2 className="text-5xl font-extrabold text-gray-900 mb-6">
@@ -23,20 +23,20 @@ export default function Home() {
               We promise every learner:
             </p>
             <ul className="text-gray-600 space-y-2 list-disc list-inside">
-            <li>A life plan they own, co-designed with mentors</li>
-            <li>
-              Real experience before they enter the workforce or higher
-              education
-            </li>
-            <li>Skills that endure even as the world shifts</li>
-            <li>
-              Belonging and purpose grounded in identity, community, and
-              contribution
-            </li>
-            <li>
-              Confidence and resilience to learn, adapt, and thrive—no matter
-              what the future holds
-            </li>
+              <li>A life plan they own, co-designed with mentors</li>
+              <li>
+                Real experience before they enter the workforce or higher
+                education
+              </li>
+              <li>Skills that endure even as the world shifts</li>
+              <li>
+                Belonging and purpose grounded in identity, community, and
+                contribution
+              </li>
+              <li>
+                Confidence and resilience to learn, adapt, and thrive—no matter
+                what the future holds
+              </li>
             </ul>
           </div>
         </div>
